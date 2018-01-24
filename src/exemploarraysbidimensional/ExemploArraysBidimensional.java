5623b2e3304b58ee01c2b224d8e2b5ac0b2eabf6
David <David@LAPTOP-VSEKM5I2>
2018-01-24 17:45:30 +0000

    COMMIT GITHUB

---
+++ src/exemploarraysbidimensional/ExemploArraysBidimensional.java
@@ -0,0 +1,27 @@
+
+package exemploarraysbidimensional;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class ExemploArraysBidimensional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Notas not=new Notas();
+        not.crearTabla();
+        not.amosar();
+        not.mediaAlumno();
+        not.mediaAlumno2();
+        not.mediaModulo();
+        not.ordenar();
+        not.amosar();
+        
+    }
+    
+}
